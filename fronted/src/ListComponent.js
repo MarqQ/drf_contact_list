1e1ead39ba2efef778fd819a8cfe324ea90a7b2f
MarqQ <marcos.w.queiroz@gmail.com>
2021-04-26 18:21:50 -0300

    React component local test and Django CORS headers included

---
+++ fronted/src/ListComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function ListComponent(){
+    return ( 
+        <div>
+            <h3>Name List</h3>
+            <ul>            
+                <li>Name 1</li>
+                <li>Name 2</li>
+            </ul>
+        </div>
+    );
+}
